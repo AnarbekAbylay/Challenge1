8f5349ab1a37dedf6d0773ed0b30e22775a3a5c9
AnarbekAbylay <iodakefir@icloud.com>
2022-03-16 13:57:33 +0500

    Initial Commit

---
+++ Challenge1(Hws)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge1(Hws)
+//
+//  Created by Abylay Anarbek on 16/3/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
